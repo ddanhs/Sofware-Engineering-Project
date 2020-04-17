2875821974b3253349bd12c88c2602fe5524c4bc
Diana <id.danhs@gmail.com>
2020-04-16 23:39:40 -0400

    profile page

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.relativelayout import RelativeLayout
+
 
 class MainApp(App):
     def build(self):
-        return GridLayout()
+        return FloatLayout()
 
 
 MainApp().run()
